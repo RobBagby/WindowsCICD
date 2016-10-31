bbc9770d528473b44000632b229e941856f3d80f
Rob Bagby <Rob@RobBagby.com>
2016-10-31 09:57:45 -0700

    Added GenericWeb MVC application that will be used for CICD pipeline

---
+++ GenericWeb/GenericWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GenericWeb.Startup))]
+namespace GenericWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
