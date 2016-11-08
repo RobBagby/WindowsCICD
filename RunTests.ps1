f0404b53e0030fc670ef9ed2289c163c6d06b766
Rob Bagby <Rob@RobBagby.com>
2016-11-08 13:47:15 -0700

    Updated the --result flag to run the tests to properly push to nunit 2 format - allows test results to be read by jenkins NUnit reader

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,2 +1,2 @@
 ﻿cd C:\build\GenericWeb\GenericWeb.Tests\bin\Debug
-.\GenericWeb.Tests.exe --out=c:\test\TestSummary.txt --result=nunit2 --work=c:\test
+.\GenericWeb.Tests.exe --out=c:\test\TestSummary.txt --result="TestResult.xml;format=nunit2" --work=c:\test
