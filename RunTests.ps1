3accc5e124c2befbe8a79aeba61245cd36145cc4
Rob Bagby <Rob@RobBagby.com>
2016-11-08 13:32:11 -0700

    Changed RunTests.ps1 to output nunit test results in nunit 2 format.  Result parser was not working in Jenkins

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,2 +1,2 @@
 ﻿cd C:\build\GenericWeb\GenericWeb.Tests\bin\Debug
-.\GenericWeb.Tests.exe --out=c:\test\TestSummary.txt --work=c:\test
+.\GenericWeb.Tests.exe --out=c:\test\TestSummary.txt --result=nunit2 --work=c:\test
