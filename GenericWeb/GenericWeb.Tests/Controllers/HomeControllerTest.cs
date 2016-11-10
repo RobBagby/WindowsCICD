cbac3ef0c1c9b453a205a87d67376b9563a419f5
Rob Bagby <Rob@RobBagby.com>
2016-11-09 17:58:02 -0700

    Made change to test webhook

--- GenericWeb/GenericWeb.Tests/Controllers/HomeControllerTest.cs
+++ GenericWeb/GenericWeb.Tests/Controllers/HomeControllerTest.cs
@@ -12,11 +12,6 @@ namespace GenericWeb.Tests.Controllers
     [TestFixture]
     public class HomeControllerTest
     {
-        [Test]
-        public void TestThatWIllFail()
-        {
-            Assert.Fail("This is a failure message");
-        }
 
         [Test]
         public void Index()
